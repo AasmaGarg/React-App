6d7005ca21685599aa46be00446852aa847d7516
Aasma Garg <aasgarg@adobe.com>
2017-12-13 15:01:44 +0530

    Route related first changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, browserHistory} from 'react-router'
+import Routes from './routes/index.js'
 
-ReactDOM.render(<div> Hello </div>, document.getElementById('root'));
+ReactDOM.render(<Router 
+					history={browserHistory}
+					routes={Routes}
+				/>, 
+				document.getElementById('root'));
